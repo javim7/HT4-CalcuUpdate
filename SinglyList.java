7040374ccba985d58a52b786593db8fdbe2a371e
javim7 <61723252+javim7@users.noreply.github.com>
2021-02-23 19:55:51 -0600

    agregando metodos

--- SinglyList.java
+++ SinglyList.java
@@ -5,5 +5,35 @@
  */
 
 public class SinglyList<E> extends AbstractSack<E> {
+
+    @Override
+    public void push(E item) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public E pop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean empty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
 }
